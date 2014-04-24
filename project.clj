f8a53fd59afba93ee9aac38bf85a3332e161648a
Tomi Suuronen <tomi.suuronen@saunalahti.com>
2014-04-24 19:56:36 +0300

    Simple street name and number parsing.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject JHS-106 "0.1.0-SNAPSHOT"
+  :description "Clojure implementation of Finnish address parsing based on JHS-106 specification."
+  :url "https://github.com/tomimas/JHS-106"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
