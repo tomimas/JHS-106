4e79810d6e90578a9e2e140422df49c5666ac6d5
Tomi Suuronen <tomi@suuronen.eu>
2014-04-24 23:42:06 +0300

    Added missing part for publishing into clojars.org

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
+  :scm {:name "git"
+        :url "https://github.com/tomimas/JHS-106"}
   :profiles {:dev {:plugins [[lein2-eclipse "2.0.0"]]}})
