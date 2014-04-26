0a5381a4794e0ff1365ef760094fa84916135e28
Tomi Suuronen <tomi@suuronen.eu>
2014-04-26 19:19:00 +0300

    Added preliminary tests where street name is shortened.

--- test/jhs_106/reg_test.clj
+++ test/jhs_106/reg_test.clj
@@ -5,16 +5,22 @@
 (deftest shouldMatch
   (testing "Street name regexp matching"
     (is (= "Rosvokatu" (re-matches streetName "Rosvokatu")))
+    (is (= "Rosvok." (re-matches streetName "Rosvok.")))
+    (is (= "Rosvot." (re-matches streetName "Rosvot.")))
+    (is (= "Rosvotr." (re-matches streetName "Rosvotr.")))
     (is (= "Urhean Rosvon katu" (re-matches streetName "Urhean Rosvon katu")))
     (is (= "Tark'ampujankatu" (re-matches streetName "Tark'ampujankatu")))
     (is (= "Tark\u00B4ampujankatu" (re-matches streetName "Tark\u00B4ampujankatu")))
+    (is (= "Tark\u00B4ampujank." (re-matches streetName "Tark\u00B4ampujank.")))
     (is (= "Gregorius X:n tie" (re-matches streetName "Gregorius X:n tie")))
     (is (= "Castr\u00E9nin polku" (re-matches streetName "Castr\u00E9nin polku")))
     (is (= "CASTR\u00C9NIN AUKIO" (re-matches streetName "CASTR\u00C9NIN AUKIO")))
     (is (= "\u00D6rkkim\u00F6rkin tie" (re-matches streetName "\u00D6rkkim\u00F6rkin tie")))
     (is (= "\u00C4mm\u00E4l\u00E4nkatu" (re-matches streetName "\u00C4mm\u00E4l\u00E4nkatu")))
     (is (= "Minna \u00C4kkijyrk\u00E4n penger" (re-matches streetName "Minna \u00C4kkijyrk\u00E4n penger")))
-    (is (= "\u00C5lans V\u00E4nstra V\u00E5ning" (re-matches streetName "\u00C5lans V\u00E4nstra V\u00E5ning"))))
+    (is (= "Minna \u00C4kkijyrk\u00E4n pgr." (re-matches streetName "Minna \u00C4kkijyrk\u00E4n pgr.")))
+    (is (= "\u00C5lans V\u00E4nstra v\u00E5ning" (re-matches streetName "\u00C5lans V\u00E4nstra v\u00E5ning")))
+    (is (= "\u00C5lans V. v\u00E5n." (re-matches streetName "\u00C5lans V. v\u00E5n."))))
   (testing "Street number regexp matching"
     (is (= "1" (re-matches streetNumber "1")))
     (is (= "1z" (re-matches streetNumber "1z")))
