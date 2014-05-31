da578b6b81ac676e9b17fbc174f74deb763d4e89
Tomi Suuronen <tomi@suuronen.eu>
2014-05-31 09:28:57 +0300

    Changed a new release number.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tomimas/jhs-106 "0.4.0-SNAPSHOT"
+(defproject tomimas/jhs-106 "0.4.0"
   :description "Clojure implementation of Finnish address parsing based on JHS-106 specification."
   :url "https://github.com/tomimas/JHS-106"
   :license {:name "Eclipse Public License"
